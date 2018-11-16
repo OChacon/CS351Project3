2f0d5f53c8fbcaad565b01949ee34bd1b2e58a4b
Oscar Chacon <orc2815@rit.edu>
2018-11-15 20:19:38 -0500

    Got some requests working
    Added requests, got some working, made sure to have it wait at least 5 sec between each one else it'll block our IP. Still don't know how he wants us to display the info so I just printed the headers of each one.

--- 351crawler.py
+++ 351crawler.py
@@ -6,9 +6,8 @@ Project 3: Simple HTTP Crawler
 """
 
 import sys
-import socket
-import math
-
+import time
+import requests
 
 def main():
     """
@@ -17,18 +16,19 @@ def main():
 
     args = sys.argv
     args_len = len(args)
+    URL = "http://wikicfp.com/cfp/call?conference=computer%20science"
     all_flag = False
     search_flag = False
     year = ""
     month = ""
 
-    if args_len == 1:
+    if args_len == 2:
         if args[1] == "dbmake|all":
             all_flag = True
         else:
             usage()
             exit(0)
-    elif args_len == 3:
+    elif args_len == 4:
         if args[1] == "dbmake|search":
             search_flag = True
             year = args[2]
@@ -40,6 +40,25 @@ def main():
         usage()
         exit(0)
 
+    r = requests.get(URL)
+    time.sleep(6)
+    r2 = requests.get(URL + "&page=2")
+    time.sleep(6)
+    r3 = requests.get(URL + "&page=3")
+    time.sleep(6)
+    r4 = requests.get(URL + "&page=4")
+    time.sleep(6)
+    r5 = requests.get(URL + "&page=5")
+
+    if all_flag:
+        print(r.headers)
+        print(r2.headers)
+        print(r3.headers)
+        print(r4.headers)
+        print(r5.headers)
+
+    if search_flag:
+        print(r.headers)
 
 def usage():
     """
@@ -51,7 +70,7 @@ def usage():
     print("\tdbmake (Required) (1)Fetches the first five pages from the WikiCFP")
     print("\t(2)Parses the Event, When, Where, and Deadline info (3)Stores it in a database.")
     print("\tall (Required) Prints out all the event, when, where, and deadline info you've fetched.")
-    print("\tsearch (Required) Takes two args for <year> and <month>. Looks update the events that match.")
+    print("\tsearch (Required) Takes two args for <year> and <month>. Queries the events that match.")
 
 
 if __name__ == "__main__":
